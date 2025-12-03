4281879497331f813267c6fc4be16c9a900311a3
mateo <mateovalente77@gmail.com>
2025-12-03 10:22:32 +0100

    Bonus

--- test_app.py
+++ test_app.py
@@ -5,3 +5,12 @@ def test_addition_positive():
 
 def test_addition_negative():
     assert addition(-1, -2) == -3
+
+    # Test des bords (zéro) [cite: 43, 45]
+def test_addition_zero():
+    assert addition(0, 0) == 0
+    assert addition(5, 0) == 5
+
+# Test des flottants [cite: 44, 47]
+def test_addition_float():
+    assert addition(2.5, 3.5) == 6.0
